3425ed9d7548819a50a0cce2300e8e5255fe2e97
Ishita345678 <ishitas227@gmail.com>
2025-11-27 11:14:18 +0530

    final Project

---
+++ pages/about.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("About the Project")
+st.write("""
+This Cooking Assistant suggests recipes based on the ingredients you have at home.
+
+### Technologies Used:
+- Python
+- Streamlit
+
+### Future Improvements:
+- Larger dataset
+- AI-based smart ranking
+- Nutrition estimation
+- Voice input support
+""")
